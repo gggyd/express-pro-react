bc5e9bfc303bd237d23c04b320d22a0746cfce22
王庆 <gggyd@live.cn>
2016-09-19 21:48:03 +0800

    initial push

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+}
+
+module.exports = {
+  entry: PATHS.src + '/App',
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: PATHS.build,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT || 3000
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        },
+        include: PATHS.src
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PATHS.src + '/index.html',
+      inject: false
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
