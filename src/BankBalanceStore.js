4ac61855b652471b1bde917150f4a13dba0f2355
王庆 <gggyd@live.cn>
2016-09-20 22:29:21 +0800

    use flux-utils

--- src/BankBalanceStore.js
+++ src/BankBalanceStore.js
@@ -1,35 +1,25 @@
-import {EventEmitter} from 'fbemitter';
+
 import AppDispatcher from './AppDispatcher';
+import {ReduceStore} from 'flux/utils';
 import bankConstants from './constants';
 
-const CHANGE_EVENT = 'change';
-let __emitter = new EventEmitter();
-let balance = 0;
+class BankBalanceStore extends ReduceStore {
+  getInitialState() {
+    return 0;
+  }
 
-let BankBalanceStore = {
-  getState() {
-    return balance;
-  },
-  addListener: (callback) => {
-    return __emitter.addListener(CHANGE_EVENT, callback);
+  reduce(state, action) {
+    switch (action.type) {
+      case bankConstants.CREATE_ACCOUNT:
+        return 0;
+      case bankConstants.DEPOSITED_INTO_ACCOUNT:
+        return state + action.ammount;
+      case bankConstants.WITHDREW_FROM_ACCOUNT:
+        return state - action.ammount;
+      default:
+        return state;
+    }
   }
 };
 
-BankBalanceStore.dispatchToken = AppDispatcher.register((action) => {
-  switch (action.type) {
-    case bankConstants.CREATE_ACCOUNT:
-      balance = 0;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.DEPOSITED_INTO_ACCOUNT:
-      balance = balance + action.ammount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.WITHDREW_FROM_ACCOUNT:
-      balance = balance - action.ammount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-  }
-});
-
-export default BankBalanceStore;
+export default new BankBalanceStore(AppDispatcher);
